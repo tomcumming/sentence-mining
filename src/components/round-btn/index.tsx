c84fc699ab5ee14eae92557064993a8aa9f1ff6a
Tom Cumming <mail@tomcumming.co.uk>
2021-06-21 21:48:52 +0100

    Add disable button styles

--- src/components/round-btn/index.tsx
+++ src/components/round-btn/index.tsx
@@ -3,12 +3,16 @@ import * as React from "react";
 export type Props = {
   icon: string;
   className?: string;
-  onClick: () => void;
+  onClick?: () => void;
 };
 
 function RoundBtn({ icon, className, onClick }: Props) {
   return (
-    <button className={`round-btn ${className || ""}`} onClick={onClick}>
+    <button
+      className={`round-btn ${className || ""}`}
+      onClick={onClick}
+      disabled={!onClick}
+    >
       {icon}
     </button>
   );
